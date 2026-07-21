4732afbe96782f492db56b6f06dbe32ee6225165
Robin Corwin <corwis@rpi.edu>
2026-07-21 17:35:58 -0400

    search page doccumentation
    yeah it's more comments!

--- app/search/index.php
+++ app/search/index.php
@@ -1,3 +1,4 @@
+<!--Howdy! welcome to the search page of the ASNM webpage! This is the big one. If you want more general info go to the home page -->
 <html lang="en">
       <head>   
         <meta charset="UTF-8">
@@ -14,7 +15,7 @@
         <title>Search</title>
       </head>
       <body>
-
+<!--Nav bar is identical to the home page -->
 <header class="navbar">
         <div class="logo" >Adaptive Sports Near Me
 
@@ -33,13 +34,12 @@
 <main class="hero">
 
     
-
     <div class="hero-content">
 
         <div class="search-area">
 
             <h2>Explore our Database</h2>
-
+            <!-- Search bar doesn't run any code yet, but eventually will run a jscript that displays database items in the appropriate order based on Clara's math, based on freshness, location, and filters-->
             <form class="search-form">
                 <input
                     type="text"
@@ -47,7 +47,7 @@
                     placeholder="Enter specifications..."
                 >
                 <select name="" id="">
-                    <option value="1">🔍 </option>
+                    <option value="1">Filter</option>
                 </select>
 
                 <button
@@ -58,6 +58,9 @@
             </form>
 
         </div>
+        <!--PHP time, This is a basic connection that connects the search page to the posgres database
+        For each "instance" (which is basically each program) there will be a block made for the appropriate instance
+        Currently the number of blocks isn't limited, but it will eventually be pages of 10 or 20 -->
         <?php $conn = pg_connect("host=localhost dbname=ASNM user=postgres password=");
 
 $query = "SELECT * FROM ASNMInstances ORDER BY sport_name, instance_name";
@@ -65,23 +68,23 @@ $result = pg_query($conn, $query);
 ?>
 
        <div class="results">
-
+<!--for every row -->
 <?php while ($row = pg_fetch_assoc($result)) { ?>
 
     <div class="sport-card">
-
+<!-- Show the name of the program-->
         <h3><?= htmlspecialchars(trim($row['instance_name'])) ?></h3>
-
+<!-- show the name of the sport -->
         <h4><?= htmlspecialchars(trim($row['sport_name'])) ?></h4>
-
+        <!-- x to y for ages-->
         <a>
             Ages: <?= $row['instance_age_floor'] ?> - <?= $row['instance_age_ceil'] ?>
         </a>
-
+        <!--description of the program, limited at 200 chars, might be made into 400 -->
         <h5>
             <?= htmlspecialchars(trim($row['instance_desc'])) ?>
         </h5>
-
+        <!-- All of the address information is shown-->
         <a>
             <?= htmlspecialchars(trim($row['instance_address_line_1'])) ?><br>
 
@@ -90,7 +93,7 @@ $result = pg_query($conn, $query);
             <?= htmlspecialchars(trim($row['instance_zip'])) ?>
 
             <br><br>
-
+        <!--Start and end dates for the program -->
             <?= $row['instance_start_month'] ?>/<?= $row['instance_start_day'] ?>/<?= $row['instance_start_year'] ?>
             -
 
@@ -98,7 +101,8 @@ $result = pg_query($conn, $query);
         </a>
 
         <h6>
-
+            <!--This is the skill level section, NULL is the false state here, if the attribute is NULL then it doesn't allow that skill level
+            and then displays what the database says -->
             <?php
             $levels = [];
 
@@ -115,14 +119,14 @@ $result = pg_query($conn, $query);
             ?>
 
             <br>
-
+            <!--Cost of the program -->
             Cost:
             <?= ($row['instance_cost'] == 0)
                 ? "Free"
                 : "$" . number_format($row['instance_cost'], 2); ?>
 
         </h6>
-
+        <!--Everything else is pretty self explainatory -->
         <a>
             Organization ID:
             <?= htmlspecialchars(trim($row['organization_id'])) ?>
