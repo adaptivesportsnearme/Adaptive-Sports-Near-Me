b18e29fc75436cdeee57dc1a06a956d19863bb17
Robin Corwin <corwis@rpi.edu>
2026-08-03 19:03:45 -0400

    Search Bar - Basic Ver
    Basic Version of the search function, does work but isn't very flexible. Also added the Newsletter footer.

--- app/search/index.php
+++ app/search/index.php
@@ -18,16 +18,13 @@
 <!--Nav bar is identical to the home page -->
 <header class="navbar">
         <div class="logo" >Adaptive Sports Near Me
-
         </div>
-
         <nav>
             <a href="../index.html">Home</a>
-            <a hred="./index.php">Search</a>
+            <a href="./index.php">Search</a>
             <a href="../about/index.html">About</a>
             <a href="#">Contact</a>
             <button class="nav-btn">List Your Program</button>
-
         </nav>
 </header>
 
@@ -39,20 +36,26 @@
         <div class="search-area">
 
             <h2>Explore our Database</h2>
-            <!-- Search bar doesn't run any code yet, but eventually will run a jscript that displays database items in the appropriate order based on Clara's math, based on freshness, location, and filters-->
-            <form class="search-form">
+            <!-- This is a very very very basic searchbar. It doesn't have any autocorrect or guesser for if the user makes a typo
+            Adding a "do you mean x? is the next step. It has a few different filters, those being All which looks through all fields
+            Sport Name and Program Name, which are self explanitory, and City which is also pretty self explainatory-->
+            <form class="search-form" method="GET" action="">
                 <input
                     type="text"
+                    name="search"
                     class="search-input"
                     placeholder="Enter specifications..."
+                    value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>"
                 >
-                <select name="" id="">
-                    <option value="1">Filter</option>
+
+                <select name="filter">
+                    <option value="all">All</option>
+                    <option value="sport">Sport Name</option>
+                    <option value="program">Program Name</option>
+                    <option value="city">City</option>
                 </select>
 
-                <button
-                    type="submit"
-                    class="search-btn">
+                <button type="submit" class="search-btn">
                     Show Me Programs
                 </button>
             </form>
@@ -61,15 +64,60 @@
         <!--PHP time, This is a basic connection that connects the search page to the posgres database
         For each "instance" (which is basically each program) there will be a block made for the appropriate instance
         Currently the number of blocks isn't limited, but it will eventually be pages of 10 or 20 -->
+        <!-- This is just a bunch of basic SQL queries that filters the results based on the filter and search query. It's very simplistic
+        because it just does an ILIKE check, but it does work, so that's really cool -->
         <?php $conn = pg_connect("host=localhost dbname=ASNM user=postgres password=");
 
-$query = "SELECT * FROM ASNMInstances ORDER BY sport_name, instance_name";
-$result = pg_query($conn, $query);
-?>
+       
+            $search = isset($_GET['search']) ? trim($_GET['search']) : "";
+            $filter = isset($_GET['filter']) ? $_GET['filter'] : "all";
+
+if ($search != "") {
+
+    switch ($filter) {
+
+        case "sport":
+            $query = "SELECT * FROM ASNMInstances
+                      WHERE sport_name ILIKE $1
+                      ORDER BY sport_name, instance_name";
+            break;
+
+        case "program":
+            $query = "SELECT * FROM ASNMInstances
+                      WHERE instance_name ILIKE $1
+                      ORDER BY sport_name, instance_name";
+            break;
+
+        case "city":
+            $query = "SELECT * FROM ASNMInstances
+                      WHERE instance_city ILIKE $1
+                      ORDER BY sport_name, instance_name";
+            break;
+
+        default:
+            $query = "SELECT * FROM ASNMInstances
+                      WHERE
+                          sport_name ILIKE $1 OR
+                          instance_name ILIKE $1 OR
+                          instance_city ILIKE $1 OR
+                          instance_desc ILIKE $1
+                      ORDER BY sport_name, instance_name";
+    }
+
+    $result = pg_query_params($conn, $query, array("%$search%"));
+
+} else {
+
+    $query = "SELECT * FROM ASNMInstances
+              ORDER BY sport_name, instance_name";
+
+    $result = pg_query($conn, $query);
+}
+        ?>
 
        <div class="results">
 <!--for every row -->
-<?php while ($row = pg_fetch_assoc($result)) { ?>
+        <?php while ($row = pg_fetch_assoc($result)) { ?>
 
     <div class="sport-card">
 <!-- Show the name of the program-->
@@ -146,6 +194,21 @@ $result = pg_query($conn, $query);
     </div>
 
 </main>
+<section class="newsletter">
+    <div class="container newsletter-inner">
+
+        <div>
+            <h3>Stay in the loop</h3>
+            <p>Get notified when new programs are added near you.</p>
+        </div>
+
+        <form class="newsletter-form">
+            <input type="email" placeholder="you@example.com">
+            <button type="submit">Subscribe →</button>
+        </form>
+
+    </div>
+</section>
 
 </body>
 </html>
